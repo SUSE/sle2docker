705cbe52e50708d93c8dda3fd1cd758482840099
Flavio Castelli <fcastelli@suse.com>
2015-03-19 17:54:29 +0100

    Move kiwi templates to dedicated folder

--- lib/sle2docker/template.rb
+++ lib/sle2docker/template.rb
@@ -3,13 +3,13 @@ module Sle2Docker
   class Template
 
     def self.list
-      Dir[File.expand_path('../../templates/*', __FILE__)].map do |dir|
+      Dir[File.expand_path('../../templates/kiwi/*', __FILE__)].map do |dir|
         File.basename(dir)
       end
     end
 
     def self.template_dir(template_name)
-      dir = File.expand_path("../../templates/#{template_name.upcase}", __FILE__)
+      dir = File.expand_path("../../templates/kiwi/#{template_name.upcase}", __FILE__)
       if !File.exists?(dir)
         raise TemplateNotFoundError.new("Cannot find template with name #{template_name}")
       end
